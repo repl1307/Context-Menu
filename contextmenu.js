737d0222aa7ec0445321130cce2f8ed717038577
repl1307 <144289085+repl1307@users.noreply.github.com>
2023-10-14 18:11:15 -0400

    Update contextmenu.js
    Menu scales better, spacing between suboptions fixed.

--- contextmenu.js
+++ contextmenu.js
@@ -1,30 +1,35 @@
 //add icon styling
-function addIconStyles() {
+(function() {
   const style = document.createElement('style');
   style.innerHTML = `
-  .icon {
-    width: 15px;
+  .context-menu--icon {
+    width: 1em;
     height: auto;
     margin: 0px 5px;
   }`;
   document.head.appendChild(style);
-}
-addIconStyles();
+})();
 
+//context menu class
 class ContextMenu {
-  constructor(options, parent = document.body, backgroundColor = 'darkgrey', color = 'white', hover = 'lighten', customStyles = '') {
-    this.backgroundColor = backgroundColor;
+  constructor(options, parent = document.body, hover = 'lighten', customStyles = null, customListItemStyles = null) {
     const chars = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMONPQRSTUVWXYZ';
+    //setting id
     this.id = '';
     for (let i = 0; i < 7; i++) { this.id += chars[Math.floor(Math.random() * (chars.length - 1))]; }
     if (!parent.dataset.menu) { parent.dataset.menu = this.id; }
-    this.color = color;
+    //custom styling 
+    this.customStyles = customStyles;
+    this.customListItemStyles = customListItemStyles;
     this.parent = parent; //element menu is attached to
     this.hover = hover;
-    this.contextMenu = this.createContextMenu(options, customStyles);
+    this.contextMenu = this.createContextMenu(options, customStyles, customListItemStyles);
     //a unique data attribute for context menus in order to prevent opening menus on top of menus
     this.contextMenu.dataset.isContextMenu = true;
     this.contextMenu.style.display = 'none';
+    this.parent.style.webkitUserSelect = 'none';
+    this.parent.style.userSelect = 'none';
+    this.parent.style.mozUserSelect = 'none';
     this.target = null; // last clicked element within parent
     //hide option menu on click
     document.addEventListener('mousedown', e => {
@@ -37,9 +42,9 @@ class ContextMenu {
       this.contextMenu.style.display = 'none';
     });
     //replace default context menu 
-    this.parent.addEventListener('contextmenu', e => {
+      this.parent.addEventListener('contextmenu', e => {
       e.preventDefault();
-      const offset = 15;
+      const offset = 2;
       let parent = e.target.parentNode;
 
       //prevent menu from opening on top of other menu
@@ -100,37 +105,47 @@ class ContextMenu {
         this.contextMenu.dataset.align = 'left';
       }
       else {
-        this.contextMenu.style.left = (e.clientX) + 'px';
+        this.contextMenu.style.left = (e.clientX+offset) + 'px';
         this.contextMenu.dataset.align = 'right';
       }
       const height = parseInt(getComputedStyle(this.contextMenu).height.replace('px', ''));
       if (parseInt(e.clientY) + height >= window.innerHeight)
         this.contextMenu.style.top = (e.clientY - height - offset) + 'px';
       else
-        this.contextMenu.style.top = (e.clientY) + 'px';
+        this.contextMenu.style.top = (e.clientY+offset) + 'px';
     });
-
   }
   //create actual context menu
-  createContextMenu = function(options = [], customStyles = null) {
+  createContextMenu = function(options = [], customStyles = null, customListItemStyles = null) {
     const container = document.createElement('ul');
-    container.style = globalContainerStyling(this.backgroundColor, this.color) + ' width: 200px;';
+    if(customStyles == null)
+      container.style = globalContainerStyling() + ' width: 200px;';
+    else
+      container.style = mandatoryContainerStyling + customStyles +'width: 200px';
+    
     // recursive function that allows as many options within options as desired
     const createOption = (option, parent, container) => {
       option.menu = this;
       option.parent = this.parent;
       option.callbackWrapper = (opt) => { this.contextMenu.style.display = 'none' };
-      const optElem = this.createListItem();
-
+      const optElem = this.createListItem('', customListItemStyles);
+      option.elem = optElem;
       optElem.dataset.isContextMenu = true;
-      const svg = option.icon;
-      optElem.appendChild(option.icon);
+      //add icon if one exists
+      if(option.icon != null){
+        const svg = option.icon;
+        optElem.appendChild(option.icon);
+      }
       optElem.innerHTML += option.name;
       optElem.onclick = option.callback;
 
       if (option.options.length > 0) {
         const innerContainer = document.createElement('ul');
-        innerContainer.style = globalContainerStyling(this.backgroundColor, this.color) + ' display: none; min-width:150px; max-width: 150px; margin-left: 5px; left: 100%;';
+        if(customStyles == null)
+          innerContainer.style = globalContainerStyling() + ' display: none; min-width:150px; max-width: 150px; left: 100%;';
+        else
+          innerContainer.style = mandatoryContainerStyling + customStyles + ' display: none; min-width:150px; max-width: 150px; left: 100%;';
+        
         optElem.onclick = null;
         for (const child of option.options) {
           createOption(child, optElem, innerContainer);
@@ -161,9 +176,13 @@ class ContextMenu {
   }
 
   //create list item
-  createListItem = function(message = '') {
+  createListItem = function(message = '', customStyles = null) {
     const li = document.createElement('li');
-    li.style = globalListItemStyling(this.color);
+    if(customStyles == null)
+      li.style = globalListItemStyling();
+    else
+      li.style = mandatoryListItemStyling + customStyles;
+      
     li.textContent = message;
     li.onmouseover = () => {
       li.dataset.hover = true;
@@ -171,29 +190,33 @@ class ContextMenu {
       while(parent.parentNode.dataset.isContextMenu){
         parent = parent.parentNode;
       }
+
+      //looping through suboptions and displaying
       for (const child of li.children) {
         if(child.nodeName == 'svg'){ continue; }
-        child.style.display = 'block';
+        child.style.display = 'flex';
         const rect = child.getBoundingClientRect();
-
-        parent.style.border = '1px solid red';
         child.dataset.align = 'right';
         //TODO: replace window width check with parent width check
         if (rect.x + rect.width > window.innerWidth) {
-        //   child.style.left = '-' + (rect.width + 10) + 'px';
-        //   console.log(child)
-        //   child.style.border = '3px solid pink'
           child.dataset.align = 'left';
         }
-        console.log(child.dataset.align);
+        else if(rect.x <= 0){
+          child.dataset.align = 'right'
+        }
+        const pRect = parent.getBoundingClientRect();
+        const paddingRight = parseFloat(getComputedStyle(parent).paddingRight.replace('px', ''));
+        const paddingLeft = parseFloat(getComputedStyle(parent).paddingLeft.replace('px', ''));
         if(parent.dataset.align == 'left')
-          child.style.left = '-' + (rect.width + 10) + 'px';
+          child.style.left = '-' + (rect.width + paddingRight) + 'px';
+        else
+          child.style.left = (child.parentNode.clientWidth + paddingLeft) + 'px';
       }
 
       if (this.hover == 'lighten')
-        li.style.backgroundColor = tinycolor(this.backgroundColor).lighten(10).toString();
+        li.style.backgroundColor = tinycolor(getComputedStyle(this.contextMenu).backgroundColor).lighten(10).toString();
       else
-        li.style.backgroundColor = tinycolor(this.backgroundColor).darken(10).toString();
+        li.style.backgroundColor = tinycolor(getComputedStyle(this.contextMenu).backgroundColor).darken(10).toString();
     
       
     };
@@ -205,12 +228,13 @@ class ContextMenu {
         let hover = li.dataset.hover.toLowerCase() == 'true';
         if (hover) { return; }
         for (const child of li.children) {
-          if (child.classList.contains('icon')) { continue; }
+          if (child.classList.contains('context-menu--icon')) { continue; }
           child.style.display = 'none';
+          child.dataset.align = 'right';
           child.style.left = '100%';
         }
       }, 150);
-      li.style.backgroundColor = this.backgroundColor;
+      li.style.backgroundColor = getComputedStyle(this.contextMenu).backgroundColor;
     };
     return li;
   }
@@ -223,10 +247,10 @@ class ContextMenu {
 }
 
 //globals styles
-const globalContainerStyling = (backgroundColor, color) => {
+const globalContainerStyling = () => {
   return `
-    background-color: ${backgroundColor};
-    color: ${color};
+    background-color: rgb(90, 90, 90);
+    color: white;
     position: absolute;
     list-style-type: none;
     display: flex;
@@ -237,9 +261,25 @@ const globalContainerStyling = (backgroundColor, color) => {
     left: 0;
     border-radius: 10px;
 `};
-const globalListItemStyling = (color) => {
+const mandatoryContainerStyling = `
+    position: absolute;
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    top: 0;
+    left: 0;
+    margin: 0;
+    padding: 0;
+`;
+const mandatoryListItemStyling = `
+    user-select: none;
+    position: relative;
+    display: block;
+    flex-direction: row;
+`;
+const globalListItemStyling = () => {
   return `
-    border: 1px solid ${color};
+    border: 1px solid white;
     padding: 5px;
     font-size: 1.15rem;
     user-select: none;
@@ -263,7 +303,7 @@ class Option {
       }
     };
     this.options = options;
-    this.icon = ContextMenuIcons.copy();
+    this.icon = null;
   }
 }
 
@@ -271,7 +311,7 @@ const ContextMenuIcons = {
   copy: () => {
     const svg = document.createElement('svg');
     svg.dataset.isContextMenu = true;
-    svg.classList.add('icon');
+    svg.classList.add('context-menu--icon');
     svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
     svg.setAttribute('viewBox', '0 0 448 512');
     const path = document.createElement('path');
@@ -280,6 +320,18 @@ const ContextMenuIcons = {
     path.dataset.isContextMenu = true;
     svg.appendChild(path);
     return svg;
-    /*'<svg class="icon" data-iscontextmenu="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path fill="currentColor" d="M384 336H192c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16l140.1 0L400 115.9V320c0 8.8-7.2 16-16 16zM192 384H384c35.3 0 64-28.7 64-64V115.9c0-12.7-5.1-24.9-14.1-33.9L366.1 14.1c-9-9-21.2-14.1-33.9-14.1H192c-35.3 0-64 28.7-64 64V320c0 35.3 28.7 64 64 64zM64 128c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H256c35.3 0 64-28.7 64-64V416H272v32c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V192c0-8.8 7.2-16 16-16H96V128H64z"/></svg>*/
+  },
+  paste: () => {
+    const svg = document.createElement('svg');
+    svg.dataset.isContextMenu = true;
+    svg.classList.add('context-menu--icon');
+    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+    svg.setAttribute('viewBox', '0 0 512 512');
+    const path = document.createElement('path');
+    path.setAttribute('fill', 'currentColor');
+    path.setAttribute('d', 'M80 96v16c0 17.7 14.3 32 32 32h60.8c16.6-28.7 47.6-48 83.2-48h62c-7.1-27.6-32.2-48-62-48H215.4C211.6 20.9 188.2 0 160 0s-51.6 20.9-55.4 48H64C28.7 48 0 76.7 0 112V384c0 35.3 28.7 64 64 64h96V400H64c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H80zm64-40a16 16 0 1 1 32 0 16 16 0 1 1 -32 0zM256 464c-8.8 0-16-7.2-16-16V192c0-8.8 7.2-16 16-16H384v48c0 17.7 14.3 32 32 32h48V448c0 8.8-7.2 16-16 16H256zm192 48c35.3 0 64-28.7 64-64V227.9c0-12.7-5.1-24.9-14.1-33.9l-51.9-51.9c-9-9-21.2-14.1-33.9-14.1H256c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H448z');
+    path.dataset.isContextMenu = true;
+    svg.appendChild(path);
+    return svg;
   },
 };
